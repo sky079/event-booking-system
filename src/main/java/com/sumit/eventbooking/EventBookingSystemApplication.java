6c69676068eed1f3664606fd6129b8d8574247ec
Sumit <sumitydv079@gmailcom>
2026-09-20 10:31:36 +0530

    Step 1: project skeleton, local postgres, health check

---
+++ src/main/java/com/sumit/eventbooking/EventBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sumit.eventbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumit/eventbooking/EventBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.eventbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
